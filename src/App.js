3213b2d7068897abd3431bb23f31f2f19f9591b8
jianren <365088870@qq.com>
2020-03-15 18:36:54 +0800

    01-搜索历史小案例

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// import Content from './components/content.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let Content = props => {
+  // console.log(props);
+  return <ul style={{ border: "1px solid #000", width: "300px" }}>
+    {props.arr.map((v,i) => <li key={i} style={{ listStyle: "none" }}>{v}</li>)}
+  </ul>
+
+}
+class App extends Component {
+  state = {
+    arr: [],
+    msg: ""
+  }
+  handleChange = (event) => {
+    // 因为浏览器性能优化问题，event点开后的target值被清空了，所以一直为null
+    // console.log(event);
+    this.setState({
+      msg: event.target.value.trim()
+    })
+
+  }
+  handleClick = () => {
+    let arr1 = this.state.arr
+    arr1.push(this.state.msg)
+    // 数组去重
+    let arr2 = [...new Set(arr1)]
+    console.log(new Set(arr1));
+    this.setState({
+      arr: arr2,
+      msg: ""
+    })
+    // console.log(this.state.arr);
+  }
+  render() {
+    return (
+      <div className="App">
+        <input value={this.state.msg} onChange={this.handleChange} type="text" />
+        <button className="btn" style={{ marginLeft: "10px" }} onClick={this.handleClick}>按钮</button>
+        <Content {...this.state}></Content>
+
+      </div >
+
+    );
+  }
 }
 
 export default App;
